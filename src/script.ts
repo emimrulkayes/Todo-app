959782236edb801a80afb7b71e35ade0472cf005
emimrulkayes <emimrulkayes@gmail.com>
2022-11-12 16:18:04 +0600

    add gitignore

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,4 @@
+const list = document.querySelector("#list") as HTMLUListElement;
+const form = document.getElementById("task_form") as HTMLFormElement | null;
+const input = document.querySelector("task_title") as HTMLInputElement;
 
-
-
-
-let a: number = 50;
-let b: number = 60;
-let sum = a + b;
-console.log(sum);
