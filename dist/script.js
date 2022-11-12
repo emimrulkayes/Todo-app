9e7960be9782e6bd0063139a5dc1eefb7408c6e4
emimrulkayes <emimrulkayes@gmail.com>
2022-11-13 00:51:56 +0600

    Added some changes

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,18 @@
-"use strict";
-let a = 50;
-let b = 60;
-let sum = a + b;
-console.log(sum);
+// import { uuid } from "../node_modules/uuidv4/build/lib/uuidv4";
+import { v4 as uuidv4 } from 'uuid';
+const list = document.querySelector("#list");
+const form = document.getElementById("task_form");
+const input = document.querySelector("task_title");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value == "" || input.value == null) {
+        alert("Please enter a task title");
+        return;
+    }
+    const task = {
+        id: uuidv4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date(),
+    };
+});
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,52 @@
-const list = document.querySelector("#list") as HTMLUListElement;
-const form = document.getElementById("task_form") as HTMLFormElement | null;
-const input = document.querySelector("task_title") as HTMLInputElement;
+// import { uuid } from "../node_modules/uuidv4/build/lib/uuidv4";
 
+import { v4 as uuidv4 } from "uuid";
+
+type Task = {
+    id: string,
+    title: string,
+    completed: boolean,
+    createdAt: Date,
+}
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("task_form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#task_title")
+const tasks: Task[] = [];
+
+form?.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (input.value == "" || input.value == null) {
+        alert("Please enter a task title");
+        return;
+    }
+
+    const newTask: Task = {
+        id: uuidv4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date(),
+    }
+    tasks.push(newTask);
+    
+
+    addListItem(newTask)
+    input.value = "";
+});
+
+function addListItem(task: Task) {
+    const item = document.createElement("li");
+    const label = document.createElement("label");
+    const checkbox = document.createElement("input");
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked;
+        console.log(tasks)
+    })
+    checkbox.type = "checkbox";
+    checkbox.checked = task.completed;
+    label.append(checkbox, task.title);
+    item.append(label);
+    list?.append(item);
+    return true;
+}
